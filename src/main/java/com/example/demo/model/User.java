02144f24d7161263161484a60559d49a64b8ae04
Ruggery <ruggeryalves@hotmail.com>
2025-12-30 11:34:27 +0000

    User.java created
    Creating user attributes and also using @Entity JPA ANNOTATIONS to create the table inside mysql with the tags. Constructors, gets and sets also created and inside pox.xml including a property to specify the main

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+/**
+ *
+ * @author Ruggery
+ */
+
+@Entity
+@Table(name = "users")
+public class User {
+    
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String email;
+    private int age;
+
+    //Empty constructor
+    public User() {
+    }
+
+    //Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    
+    
+}
