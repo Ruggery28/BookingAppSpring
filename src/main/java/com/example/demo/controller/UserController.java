6c178180eeb71e5f8d33fad3cb42566dfdfb5001
Ruggery <ruggeryalves@hotmail.com>
2026-01-02 11:41:04 +0000

    Create UserController
    test: added manual user creation endpoint

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Ruggery
+ */
+@RestController
+@RequestMapping("/users") //the URL for this class will always be users.
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    //constructor to receive a userRepository as parameter
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userRepository.findAll(); //this is a method from interface that writes a SELECT * from users.
+    }
+    
+    //method to test if user will be saved and print on the webpage
+    @GetMapping("/add")
+    public String addUser(){
+        User temp = new User();
+        temp.setName("Ruggery");
+        userRepository.save(temp);
+        return "User saved successfully!";
+    }
+}
